54f42a81e0137a30225d5ec12cc75facba7cd086
Jaco Greeff <jacogr@gmail.com>
2017-11-23 18:30:28 +0100

    Documentation generation from root files (#10)

---
+++ src/testRoot.js
@@ -0,0 +1,14 @@
+// ISC, Copyright 2017 Jaco Greeff
+// @flow
+
+/**
+  @name test
+  @signature test (): boolean
+  @summary This is the summary for test, a root file
+  @description
+    This is the description with another line
+  @example
+    const test = require('./test');
+
+    test(); // => nothing
+*/
