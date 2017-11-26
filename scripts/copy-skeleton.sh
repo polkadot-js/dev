6af3e378e16892c4e53f8e99cae4b73b9a68f0de
Jaco Greeff <jacogr@gmail.com>
2017-11-26 12:51:00 +0100

    Copy .eslintrc across

--- scripts/copy-skeleton.sh
+++ scripts/copy-skeleton.sh
@@ -5,5 +5,5 @@ set -e
 
 SRC=node_modules/@polkadot/dev/skeleton
 
-cp -fv $SRC/.babelrc $SRC/.coveralls.yml $SRC/.flowconfig $SRC/.gitignore $SRC/.npmignore $SRC/.stylelintrc.json $SRC/.travis.yml $SRC/jest.config.js .
+cp -fv $SRC/.babelrc $SRC/.eslintrc.json $SRC/.flowconfig $SRC/.gitignore $SRC/.npmignore $SRC/.stylelintrc.json $SRC/.travis.yml $SRC/jest.config.js .
 cp -fv $SRC/../.coveralls.yml $SRC/../.editorconfig .
