53a82031aedbbbf3718c2332ced81e382f0da963
Jaco Greeff <jacogr@gmail.com>
2017-11-28 16:17:11 +0100

    Add eslintignore

--- scripts/polkadot-dev-build-babel.sh
+++ scripts/polkadot-dev-build-babel.sh
@@ -3,29 +3,48 @@
 
 set -e
 
-echo ""
-echo "*** Cleaning build directory"
-
-yarn run rimraf build
-
-echo ""
-echo "*** Compiling via babel"
-
-yarn run babel --out-dir build --ignore '*.spec.js' src
-
-echo ""
-echo "*** Copying flow types (source)"
-
-yarn run flow-copy-source --verbose --ignore '*.spec.js' src build
-
-if [ -d "flow-typed" ]; then
+function build_js () {
   echo ""
-  echo "*** Copying flow types (libraries)"
+  echo "*** Cleaning build directory"
 
-  cp -r flow-typed build
+  rm -rf build
+
+  echo ""
+  echo "*** Compiling via babel"
+
+  yarn run babel --out-dir build --ignore '*.spec.js' src
+
+  echo ""
+  echo "*** Copying flow types (source)"
+
+  yarn run flow-copy-source --verbose --ignore '*.spec.js' src build
+
+  if [ -d "flow-typed" ]; then
+    echo ""
+    echo "*** Copying flow types (libraries)"
+
+    cp -r flow-typed build
+  fi
+
+  echo ""
+  echo "*** Build completed"
+}
+
+if [ -d "packages" ]; then
+  PACKAGES=( $(ls -1d packages/*) )
 fi
 
-echo ""
-echo "*** Build completed"
+if [ -n "$PACKAGES" ]; then
+  for PACKAGE in "${PACKAGES[@]}"; do
+    echo ""
+    echo "*** Executing in $PACKAGE"
+
+    cd $PACKAGE
+    build_js
+    cd ../..
+  done
+else
+  build_js
+fi
 
 exit 0
