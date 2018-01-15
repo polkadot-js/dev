f3fdf01fac91d63fa143539e36b8b7d12ce60ab7
Jaco Greeff <jacogr@gmail.com>
2018-01-15 21:45:44 +0100

    Fix flow check (#69)
    * Update default flowconfig
    
    * Remove non-existent coveralls.yml
    
    * Remove lerna bootstrap

--- scripts/polkadot-dev-copy-skeleton.sh
+++ scripts/polkadot-dev-copy-skeleton.sh
@@ -7,7 +7,7 @@ set -e
 
 SRC=node_modules/@polkadot/dev/skeleton
 
-DOTS=( "babelrc" "codeclimate.yml" "coveralls.yml" "editorconfig" "eslintignore" "eslintrc.json" "flowconfig" "gitignore" "npmignore" "stylelintrc.json" "travis.yml" )
+DOTS=( "babelrc" "codeclimate.yml" "editorconfig" "eslintignore" "eslintrc.json" "flowconfig" "gitignore" "npmignore" "stylelintrc.json" "travis.yml" )
 COPY=( "jest.config.js" "lerna.json" )
 
 for FILE in ${DOTS[@]}
