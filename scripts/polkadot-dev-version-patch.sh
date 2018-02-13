4d387fe6cbdd5e36396d431150ad22e44fcf9197
Jaco Greeff <jacogr@gmail.com>
2018-02-13 09:26:17 +0100

    Add version bump (patch) (#81)

---
+++ scripts/polkadot-dev-version-patch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Copyright 2017-2018 Jaco Greeff
+# This software may be modified and distributed under the terms
+# of the ISC license. See the LICENSE file for details.
+
+set -e
+
+if [ -d "packages" ]; then
+  lerna publish --skip-git --skip-npm --yes --cd-version patch
+fi
