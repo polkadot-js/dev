086e35bea544e38d03772482674d5f75000178c7
Jaco Greeff <jacogr@gmail.com>
2018-02-20 17:01:18 +0100

    Typo

--- flow-typed/blakejs.js
+++ flow-typed/blakejs.js
@@ -2,7 +2,7 @@
 
 declare module 'blakejs' {
   declare module.exports: {
-    blake2b: (data: Uint8Array, key?: Uint8Array | null, outlen?: number) => Uint8Array
+    blake2b: (data: Uint8Array, key?: Uint8Array | null, outlen?: number) => Uint8Array,
     blake2s: (data: Uint8Array, key?: Uint8Array | null, outlen?: number) => Uint8Array
   }
 }
