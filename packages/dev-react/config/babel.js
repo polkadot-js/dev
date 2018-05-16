c3ef967995823addf1631cfa7f60553a76e008ca
Jaco Greeff <jacogr@gmail.com>
2018-05-16 14:23:43 +0200

    react-hot-loader only in !production

--- packages/dev-react/config/babel.js
+++ packages/dev-react/config/babel.js
@@ -1,9 +1,15 @@
 const base = require('@polkadot/dev/config/babel');
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 module.exports = Object.keys(base).reduce((config, key) => {
   config[key] = base[key];
 
-  if (key === 'presets') {
+  if (isDev && key === 'plugins') {
+    config[key] = config[key].concat([
+      'react-hot-loader/babel'
+    ]);
+  } else if (key === 'presets') {
     const env = config[key].find((item) => item[0] === '@babel/preset-env');
 
     env[1].targets.browsers = [
