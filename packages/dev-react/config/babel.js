b3081e6e598c583cd1b4ad3741d06b7f6d429bb4
Jaco Greeff <jacogr@gmail.com>
2018-05-16 14:21:55 +0200

    Remove react-hot-loader from default deps

--- packages/dev-react/config/babel.js
+++ packages/dev-react/config/babel.js
@@ -3,11 +3,7 @@ const base = require('@polkadot/dev/config/babel');
 module.exports = Object.keys(base).reduce((config, key) => {
   config[key] = base[key];
 
-  if (key === 'plugins') {
-    config[key] = config[key].concat([
-      'react-hot-loader/babel'
-    ]);
-  } else if (key === 'presets') {
+  if (key === 'presets') {
     const env = config[key].find((item) => item[0] === '@babel/preset-env');
 
     env[1].targets.browsers = [
