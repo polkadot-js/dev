83ead5b108d6a90e1986d558f1667cf41f257615
Jaco Greeff <jacogr@gmail.com>
2018-06-11 10:00:05 +0200

    Update color definition

--- packages/dev/flow-typed/color.js
+++ packages/dev/flow-typed/color.js
@@ -2,12 +2,17 @@
 
 declare type color$Color = {
   alpha: (alpha: number) => color$Color,
+  ansi256: () => color$Color,
+  array: () => Array<number>,
+  cmyk: () => color$Color,
+  hsl: () => color$Color,
+  lighten: (alpha: number) => color$Color,
   rotate: (degrees: number) => color$Color,
   string: () => string
 };
 
 declare module 'color' {
   declare module.exports: {
-    (hex: string): color$Color;
+    (value: string | Array<number> | { b: number, g: number, r: number }): color$Color;
   }
 }
