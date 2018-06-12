20cab53ecc43f89a744d3c609cf7213831aec36d
Jaco Greeff <jacogr@gmail.com>
2018-06-12 12:57:09 +0200

    Error when yarn is not used

---
+++ packages/dev/scripts/polkadot-dev-yarn-only.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+// Copyright 2017-2018 Jaco Greeff
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+const chalk = require('chalk');
+
+if (process.env.npm_execpath.indexOf('yarn') === -1) {
+  const blank = ''.padStart(75);
+
+  console.error(
+    chalk.white.bold.bgRed(
+      `${blank}\n   ${chalk.bold('FATAL: The use of yarn is required, install via npm is not supported.')}   \n${blank}`
+    )
+  );
+  console.error(`\n   Technical explanation: All the projects in the ${chalk.bold('@polkadot')} family use \n   yarn workspaces, along with hoisting of dependencies. Currently only\n   yarn supports package.json workspaces, hence the limitation.\n\n\n   If yarn is not available, you can get it from https://yarnpkg.com/\n\n\n`);
+
+  process.exit(1);
+}
+
+process.exit(0);
