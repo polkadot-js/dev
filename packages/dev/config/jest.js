3161cdbc9cde95dfc31e0528ae85b5bfa8d8d352
Jaco Greeff <jacogr@gmail.com>
2018-06-26 16:42:11 +0200

    Use jest default extensions

--- packages/dev/config/jest.js
+++ packages/dev/config/jest.js
@@ -2,12 +2,14 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
+const { defaults } = require('jest-config');
+
 module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   },
   moduleFileExtensions: [
-    'js', 'jsx', 'ts', 'tsx', 'json'
+    'ts', 'tsx', ...defaults.moduleFileExtensions
   ],
   verbose: true
 };
