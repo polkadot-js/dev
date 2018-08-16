7720a757a846bf3a70f9bde240e949673b4a9f19
Jaco Greeff <jacogr@gmail.com>
2018-08-16 10:40:18 +0200

    Update dependencies

--- packages/dev-react/config/babel.js
+++ packages/dev-react/config/babel.js
@@ -4,15 +4,13 @@
 
 const base = require('@polkadot/dev/config/babel');
 
-const isDev = process.env.NODE_ENV !== 'production';
-
 module.exports = Object.keys(base).reduce((config, key) => {
   config[key] = base[key];
 
-  if (isDev && key === 'plugins') {
-    // config[key] = config[key].concat([
-    //   'react-hot-loader/babel'
-    // ]);
+  if (key === 'plugins') {
+    config[key] = config[key].concat([
+      '@babel/plugin-syntax-dynamic-import'
+    ]);
   } else if (key === 'presets') {
     config[key] = config[key].concat([
       '@babel/preset-react'
