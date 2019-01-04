1a861154717cc0deda6981927b51f828de621391
Jaco Greeff <jacogr@gmail.com>
2019-01-04 12:56:54 +0100

    2019

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
-// Copyright 2017-2018 Jaco Greeff
+// Copyright 2017-2019 @polkadot/dev authors & contributors
 // This software may be modified and distributed under the terms
-// of the ISC license. See the LICENSE file for details.
-// @flow
+// of the Apache-2.0 license. See the LICENSE file for details.
 
 module.exports = {
   extends: '@polkadot/dev-react/config/babel'
