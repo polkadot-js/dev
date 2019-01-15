f8ab5c59542fc2d46dcf059dcee66e4a0f7655cc
Jaco Greeff <jacogr@gmail.com>
2019-01-15 09:51:06 +0100

    decoratorsBeforeExport: false

--- packages/dev/config/babel.js
+++ packages/dev/config/babel.js
@@ -6,7 +6,7 @@ const isTest = process.env.NODE_ENV === 'test';
 
 const plugins = [
   ['@babel/plugin-proposal-class-properties', { loose: true }],
-  '@babel/plugin-proposal-decorators',
+  ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: false }],
   '@babel/plugin-proposal-object-rest-spread',
   '@babel/plugin-transform-runtime'
 ];
