c606e70111c48eb20f3a8c31adc13a35bc8bd1a7
Jaco Greeff <jacogr@gmail.com>
2019-01-15 10:00:33 +0100

    Add decorators transform, bump deps (#172)
    * Add decorators transform, bump deps
    
    * decoratorsBeforeExport: false
    
    * Decorator before class properties

--- packages/dev/config/babel.js
+++ packages/dev/config/babel.js
@@ -5,6 +5,8 @@
 const isTest = process.env.NODE_ENV === 'test';
 
 const plugins = [
+  // ordering important, decorators before class properties
+  ['@babel/plugin-proposal-decorators', { legacy: true }],
   ['@babel/plugin-proposal-class-properties', { loose: true }],
   '@babel/plugin-proposal-object-rest-spread',
   '@babel/plugin-transform-runtime'
