be1c632e85cecc4a96eb641110e3abae71627631
Jaco Greeff <jacogr@gmail.com>
2019-09-05 10:35:36 +0200

    Bump deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
-// Copyright 2017-2019 @polkadot/dev-react authors & contributors
-// This software may be modified and distributed under the terms
-// of the Apache-2.0 license. See the LICENSE file for details.
-
 const base = require('@polkadot/dev-react/config/eslint');
 
 module.exports = {
   ...base,
   parserOptions: {
     ...base.parserOptions,
-    extraFileExtensions: ['*.d.ts'],
     project: [
       './tsconfig.eslint.json'
     ]
