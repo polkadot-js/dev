31a987a4fcbd70f41f3455d07624d0bc53805aaf
Jaco Greeff <jacogr@gmail.com>
2019-10-22 10:50:19 +0200

    Bump deps, remove unused scripts (#250)
    * Bump deps, remove unused scripts
    
    * Bump styled-components

--- packages/dev-react/skeleton/jest.config.js
+++
@@ -1,6 +0,0 @@
-// Copyright 2017-2019 @polkadot/dev-react authors & contributors
-// This software may be modified and distributed under the terms
-// of the Apache-2.0 license. See the LICENSE file for details.
-// @flow
-
-module.exports = require('@polkadot/dev-react/config/jest');
