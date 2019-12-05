ce9ad5c549602e63082b3c9f811a181cad05e457
Jaco Greeff <jacogr@gmail.com>
2019-12-05 09:51:19 +0100

    Bump deps (#254)

--- packages/dev/config/eslint.js
+++ packages/dev/config/eslint.js
@@ -2,19 +2,24 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+const BASE_RULES = [
+  'eslint:recommended',
+  'semistandard',
+  'plugin:@typescript-eslint/eslint-recommended',
+  'plugin:@typescript-eslint/recommended'
+];
+
+if (!process.env.SKIP_ESLINT_TYPE) {
+  BASE_RULES.push('plugin:@typescript-eslint/recommended-requiring-type-checking');
+}
+
 module.exports = {
   env: {
     browser: true,
     jest: true,
     node: true
   },
-  extends: [
-    'eslint:recommended',
-    'semistandard',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
-  ],
+  extends: BASE_RULES,
   overrides: [
     {
       files: ['*.js', '*.spec.js'],
