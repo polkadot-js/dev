aff9b5029afa3d1378d5db297f83b9db66fabe54
Jaco Greeff <jacogr@gmail.com>
2020-02-13 15:36:49 +0100

    Combine version scripts

---
+++ packages/dev/scripts/polkadot-dev-version.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+// Copyright 2017-2020 @polkadot/dev authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const lerna = require('lerna/core/lerna');
+const { type } = require('yargs')
+  .options({
+    type: {
+      choices: ['major', 'minor', 'patch', 'preminor', 'prerelease'],
+      description: 'The type of version adjustment to apply',
+      required: true,
+      type: 'string'
+    }
+  })
+  .strict()
+  .argv;
+
+lerna(
+  ['version', type]
+    .concat(
+      ['preminor', 'prerelease'].includes(type)
+        ? ['--preid', 'beta']
+        : []
+    )
+    .concat(['--yes', '--no-git-tag-version', '--no-push', '--allow-branch', '*'])
+);
