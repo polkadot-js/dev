f89f3706290ab06256167adffabe2fcaf29a8cbd
Jaco Greeff <jacogr@gmail.com>
2020-02-16 13:15:48 +0100

    Add polkadot-dev-circular (#287)

---
+++ packages/dev/src/test1/circ1.ts
@@ -0,0 +1,9 @@
+// Copyright 2017-2020 @polkadot/dev authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import circ2 from './circ2';
+
+export default function circ1 (): void {
+  circ2();
+}
