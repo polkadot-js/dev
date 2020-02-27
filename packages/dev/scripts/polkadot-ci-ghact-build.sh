d522b37ac967cc6ae78c391c427a8e220e0d9de5
Jaco Greeff <jacogr@gmail.com>
2020-02-27 20:37:10 +0100

    Correct tag format

--- packages/dev/scripts/polkadot-ci-ghact-build.sh
+++ packages/dev/scripts/polkadot-ci-ghact-build.sh
@@ -173,7 +173,7 @@ function npm_publish () {
   # yarn config set registry "https://$NPMREG"
 
   while true; do
-    (npm publish --access public --tag $TAG) && break || {
+    (npm publish --access public $TAG) && break || {
       if [[ $n -lt 5 ]]; then
         echo "Publish failed on attempt $n/5. Retrying in 15s."
         ((n++))
