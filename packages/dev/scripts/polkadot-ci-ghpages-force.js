eca62c877ceb096cf2c9e852ecd1a615f95b8dbc
Jaco Greeff <jacogr@gmail.com>
2020-02-28 11:25:46 +0100

    ghpages force js wrapper

---
+++ packages/dev/scripts/polkadot-ci-ghpages-force.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+// Copyright 2017-2020 @polkadot/dev authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { execSync } = require('child_process');
+const path = require('path');
+
+function main () {
+  execSync(`${path.join(__dirname, 'polkadot-ci-ghpages-force.sh')}`, { stdio: 'inherit' });
+}
+
+main();
