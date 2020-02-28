bdd58faec018048bc393f872729b4b56b99bf917
Jaco Greeff <jacogr@gmail.com>
2020-02-28 07:52:35 +0100

    Fixup clean dirs script

--- packages/dev/scripts/polkadot-dev-clean-build.js
+++ packages/dev/scripts/polkadot-dev-clean-build.js
@@ -8,16 +8,28 @@ const fs = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
 
-const ROOT = path.join(process.cwd(), 'packages');
+const PKGS = path.join(process.cwd(), 'packages');
+
+function getDirs (dir) {
+  return [path.join(dir, 'build'), path.join(dir, 'build-docs')];
+}
+
+function cleanDirs (dirs) {
+  dirs.forEach((dir) => rimraf.sync(dir));
+}
 
 function main () {
-  fs
-    .readdirSync(ROOT)
-    .map((file) => path.join(ROOT, file))
-    .filter((file) => fs.statSync(file).isDirectory())
-    .concat(['.'])
-    .reduce((arr, dir) => arr.concat(path.join(dir, 'build'), path.join(dir, 'build-docs')), [])
-    .forEach((dir) => rimraf.sync(dir));
+  cleanDirs(getDirs(path.join(process.cwd(), 'build')));
+
+  if (fs.existsSync(PKGS)) {
+    cleanDirs(
+      fs
+        .readdirSync(PKGS)
+        .map((file) => path.join(PKGS, file))
+        .filter((file) => fs.statSync(file).isDirectory())
+        .reduce((arr, dir) => arr.concat(...getDirs(dir)), [])
+    );
+  }
 }
 
 main();
