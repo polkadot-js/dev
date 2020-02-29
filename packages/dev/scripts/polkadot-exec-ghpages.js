69d3e45a602ce36000cad39f4c2ff4b792d1d396
Jaco Greeff <jacogr@gmail.com>
2020-02-29 09:59:23 +0100

    gh-pages publish

--- packages/dev/scripts/polkadot-exec-ghpages.js
+++ packages/dev/scripts/polkadot-exec-ghpages.js
@@ -4,4 +4,10 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-require('gh-pages/bin/gh-pages');
+require('gh-pages/bin/gh-pages')(process.argv)
+  .then(() => {
+    process.stdout.write('Published\n');
+  })
+  .catch(err => {
+    process.stderr.write(`${err.message}\n`, () => process.exit(1));
+  });
