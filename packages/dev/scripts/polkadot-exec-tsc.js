48fd93093326ea26b258e0cdbd5ad18e7e22229d
Jaco Greeff <jacogr@gmail.com>
2020-03-04 15:24:59 +0100

    No tsc/typedoc via pnpify

--- packages/dev/scripts/polkadot-exec-tsc.js
+++ packages/dev/scripts/polkadot-exec-tsc.js
@@ -3,8 +3,10 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-const execSync = require('./execSync');
+// const execSync = require('./execSync');
 
-execSync(`yarn pnpify tsc ${process.argv.slice(2).join(' ')}`);
+// execSync(`yarn pnpify tsc ${process.argv.slice(2).join(' ')}`);
 
-// require('typescript/lib/tsc');
+console.log('$ tsc', process.argv.slice(2).join(' '));
+
+require('typescript/lib/tsc');
