9f19e47e0365f22a6fe39d29275dc52c3f3a5bc6
Jaco Greeff <jacogr@gmail.com>
2020-03-04 20:52:05 +0100

    Update

--- packages/dev/scripts/polkadot-exec-tsc.js
+++ packages/dev/scripts/polkadot-exec-tsc.js
@@ -3,10 +3,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// const execSync = require('./execSync');
-
-// execSync(`yarn pnpify tsc ${process.argv.slice(2).join(' ')}`);
-
 console.log('$ tsc', process.argv.slice(2).join(' '));
 
-require('typescript/lib/tsc');
+require('typescript/bin/tsc');
