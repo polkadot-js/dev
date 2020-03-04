aea9942d90b5db8afa2bdbea0d095294015c35fe
Jaco Greeff <jacogr@gmail.com>
2020-03-04 09:35:28 +0100

    Execute scripts via yarn (#307)
    * Fix linting (unused path)
    
    * Execute js scripts via yarn
    
    * Quotes

--- packages/dev/scripts/polkadot-dev-run-lint.js
+++ packages/dev/scripts/polkadot-dev-run-lint.js
@@ -4,7 +4,6 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 const { execSync } = require('child_process');
-const path = require('path');
 const argv = require('yargs')
   .options({
     'skip-eslint': {
@@ -20,9 +19,9 @@ const argv = require('yargs')
   .argv;
 
 if (!argv['skip-eslint']) {
-  execSync(`${require.resolve('@polkadot/dev/scripts/polkadot-exec-eslint.js')} --resolve-plugins-relative-to ${__dirname} --ext .js,.jsx,.ts,.tsx ${process.cwd()}`, { stdio: 'inherit' });
+  execSync(`yarn polkadot-exec-eslint --resolve-plugins-relative-to ${__dirname} --ext .js,.jsx,.ts,.tsx ${process.cwd()}`, { stdio: 'inherit' });
 }
 
 if (!argv['skip-tsc']) {
-  execSync(`${require.resolve('@polkadot/dev/scripts/polkadot-exec-tsc.js')} --noEmit --pretty`, { stdio: 'inherit' });
+  execSync('yarn polkadot-exec-tsc --noEmit --pretty', { stdio: 'inherit' });
 }
