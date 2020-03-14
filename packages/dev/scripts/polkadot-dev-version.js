8de90e0b2f2f598788017ebf6c98e30ad8833c72
Jaco Greeff <jacogr@gmail.com>
2020-03-14 08:56:54 +0100

    Fix execSync import

--- packages/dev/scripts/polkadot-dev-version.js
+++ packages/dev/scripts/polkadot-dev-version.js
@@ -3,7 +3,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-const { execSync } = require('./execSync');
+const execSync = require('./execSync');
 const { type } = require('yargs')
   .options({
     type: {
