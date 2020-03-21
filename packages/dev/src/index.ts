e2f9a5d6d40a14d3bc694142f1c582ab1c500162
Jaco Greeff <jacogr@gmail.com>
2020-03-21 07:07:18 +0100

    Bump deps (#324)
    * Bump deps
    
    * Type import test

--- packages/dev/src/index.ts
+++ packages/dev/src/index.ts
@@ -2,13 +2,15 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import type { EchoString } from './types';
+
 const A = 123;
 let count = 0;
 
 /**
  * This is just a test file to test the doc generation
  */
-export const echo = (value: string): string => {
+export const echo = (value: EchoString): string => {
   count++;
 
   return `${count}: ${A}: ${value}`;
