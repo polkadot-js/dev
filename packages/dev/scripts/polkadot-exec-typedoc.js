a1dc11ffcc66e5b79f5f5b635ef58d2289c478cb
Jaco Greeff <jacogr@gmail.com>
2020-08-26 10:24:19 +0200

    typedoc execute

--- packages/dev/scripts/polkadot-exec-typedoc.js
+++ packages/dev/scripts/polkadot-exec-typedoc.js
@@ -3,12 +3,12 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// const execSync = require('./execSync');
+const execSync = require('./execSync');
 
-// execSync(`yarn pnpify typedoc ${process.argv.slice(2).join(' ')}`);
+execSync(`yarn typedoc ${process.argv.slice(2).join(' ')}`);
 
-console.log('$ typedoc', process.argv.slice(2).join(' '));
+// console.log('$ typedoc', process.argv.slice(2).join(' '));
 
-const td = require('typedoc/dist/lib/cli.js');
+// const td = require('typedoc/dist/lib/cli.js');
 
-new td.CliApplication().bootstrap();
+// new td.CliApplication().bootstrap();
