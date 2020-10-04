a6f3414199578ed2f83655dab7b6a86b889895be
Jaco Greeff <jacogr@gmail.com>
2020-10-04 15:00:12 +0200

    Drop lerna dependency (#395)

--- packages/dev/scripts/polkadot-exec-lerna.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-// Copyright 2017-2020 @polkadot/dev authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-const args = process.argv.slice(2);
-
-console.log('$ lerna', args.join(' '));
-
-require('lerna')(args);
