7a00bae9145e4827f3e708c39d0935cffed73a04
Jaco Greeff <jacogr@gmail.com>
2020-10-05 00:23:48 +0200

    yarn version parallel

--- packages/dev/scripts/polkadot-dev-version.js
+++ packages/dev/scripts/polkadot-dev-version.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const { type } = require('yargs')
   .options({
     type: {
-      choices: ['major', 'minor', 'patch', 'prerelease'],
+      choices: ['major', 'minor', 'patch', 'prerelease', 'preminor'],
       description: 'The type of version adjustment to apply',
       required: true,
       type: 'string'
@@ -20,7 +20,7 @@ const execSync = require('./execSync');
 console.log('$ polkadot-dev-version', process.argv.slice(2).join(' '));
 
 if (fs.existsSync('packages')) {
-  execSync(`yarn workspaces foreach version ${type}`);
+  execSync(`yarn workspaces foreach --parallel --topological version ${type}`);
 } else {
   execSync(`yarn version ${type}`);
 }
