97303fe16f8841afbea0a3a2e6e02c7144b5b273
Jaco Greeff <jacogr@gmail.com>
2020-10-17 09:57:58 +0200

    Add typedoc config file

---
+++ packages/dev/config/typedoc.js
@@ -0,0 +1,18 @@
+// Copyright 2017-2020 @polkadot/dev authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+module.exports = {
+  exclude: '**/*+(index|e2e|spec|types).ts',
+  excludeExternals: true,
+  excludeNotExported: true,
+  excludePrivate: true,
+  excludeProtected: true,
+  hideGenerator: true,
+  includeDeclarations: false,
+  module: 'commonjs',
+  moduleResolution: 'node',
+  name: 'polkadot{.js}',
+  out: 'docs',
+  stripInternal: 'true',
+  theme: 'markdown'
+};
