dcd9f859f5db4cc61b33fc59b67400687bf45ebe
Jaco Greeff <jacogr@gmail.com>
2020-10-24 15:36:56 +0200

    Direct bump via package.json

--- packages/dev/scripts/polkadot-dev-version.js
+++ packages/dev/scripts/polkadot-dev-version.js
@@ -18,17 +18,48 @@ const { type } = require('yargs')
 
 const execSync = require('./execSync');
 
+function updateDependencies (dependencies, others, version) {
+  return Object
+    .entries(dependencies)
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .reduce((result, [key, value]) => {
+      result[key] = others.includes(key) && value !== '*'
+        ? `^${version}`
+        : value;
+
+      return result;
+    }, {});
+}
+
 console.log('$ polkadot-dev-version', process.argv.slice(2).join(' '));
 
 execSync(`yarn version ${type}`);
 
+// yarn workspaces does an OOM, manual looping takes ages
 if (fs.existsSync('packages')) {
-  const { version } = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'));
-
-  fs
+  const packages = fs
     .readdirSync('packages')
     .map((dir) => path.join(process.cwd(), 'packages', dir, 'package.json'))
     .filter((pkgPath) => fs.existsSync(pkgPath))
-    .map((pkgPath) => JSON.parse(fs.readFileSync(pkgPath, 'utf8')))
-    .forEach((pkgJson) => execSync(`yarn workspace ${pkgJson.name} version ${version}`));
+    .map((pkgPath) => [pkgPath, JSON.parse(fs.readFileSync(pkgPath, 'utf8'))]);
+  const others = packages.map(([, json]) => json.name);
+  const { version } = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'));
+
+  packages.forEach(([pkgPath, json]) => {
+    const updated = Object.keys(json).reduce((result, key) => {
+      if (key === 'version') {
+        result[key] = version;
+      } else if (['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies', 'resolutions'].includes(key)) {
+        result[key] = updateDependencies(json[key], others, version);
+      } else {
+        result[key] = json[key];
+      }
+
+      return result;
+    }, {});
+
+    fs.writeFileSync(pkgPath, `${JSON.stringify(updated, null, 2)}\n`);
+  });
 }
+
+execSync('yarn');
