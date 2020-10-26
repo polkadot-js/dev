0bddeeff13b3d3d5c04917c077b44f55977e61f2
Jaco Greeff <jacogr@gmail.com>
2020-10-26 10:13:19 +0100

    Add shared jest resolver

---
+++ packages/dev/config/jest-resolver.js
@@ -0,0 +1,10 @@
+// Copyright 2017-2020 @polkadot/dev authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+const path = require('path');
+
+module.exports = function resolver (file, config) {
+  return file.includes('package.json')
+    ? path.join(config.basedir.replace('/src', '/'), file)
+    : config.defaultResolver(file, config);
+};
