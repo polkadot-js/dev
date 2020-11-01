522297066331b29aab9b33a7796e389576e1dffd
Jaco Greeff <jacogr@gmail.com>
2020-11-01 18:30:52 +0100

    crypto import

--- packages/dev/config/jest.js
+++ packages/dev/config/jest.js
@@ -1,6 +1,7 @@
 // Copyright 2017-2020 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+const crypto = require('crypto');
 const { defaults } = require('jest-config');
 
 module.exports = {
