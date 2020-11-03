0378eacf20fb2585cdd65f3463b55585225cc76e
Jaco Greeff <jacogr@gmail.com>
2020-11-03 09:41:52 +0100

    Adjust getRandomValues

--- packages/dev/config/jest-crypto.js
+++ packages/dev/config/jest-crypto.js
@@ -5,6 +5,13 @@ const crypto = require('crypto');
 
 Object.defineProperty(global.self, 'crypto', {
   value: {
-    getRandomValues: (arr) => crypto.randomBytes(arr.length)
+    getRandomValues: (arr) =>
+      crypto
+        .randomBytes(arr.length)
+        .reduce((arr, value, index) => {
+          arr[index] = value;
+
+          return arr;
+        }, arr)
   }
 });
