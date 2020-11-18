e8a8a5016aba5cdaa1865684ffbce930fd8de282
Jaco Greeff <jacogr@gmail.com>
2020-11-18 14:47:22 +0100

    Add react/jsx-fragments rule

--- packages/dev/config/eslint.cjs
+++ packages/dev/config/eslint.cjs
@@ -81,6 +81,7 @@ module.exports = {
     ],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
+    'react/jsx-fragments': 'error',
     'react/jsx-max-props-per-line': [2, {
       maximum: 1,
       when: 'always'
