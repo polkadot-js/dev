a0379ce271aa4e8af65e549276a7018dc7d2d21b
Jaco Greeff <jacogr@gmail.com>
2020-11-18 14:51:47 +0100

    Add react/jsx-fragments rule (#415)
    * Add react/jsx-fragments rule
    
    * dedupe

--- packages/dev/config/babel-presets.cjs
+++ packages/dev/config/babel-presets.cjs
@@ -3,14 +3,14 @@
 
 const resolver = require('./babel-resolver.cjs');
 
-module.exports = function (moduleType) {
+module.exports = function (modules) {
   return resolver([
     '@babel/preset-typescript',
     ['@babel/preset-react', {
       runtime: 'automatic'
     }],
     ['@babel/preset-env', {
-      modules: moduleType,
+      modules,
       targets: {
         browsers: '>0.25% and last 2 versions and not ie 11 and not OperaMini all',
         node: '12'
