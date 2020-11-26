b99c948054d12ffdc34ae67485c36e943d489df5
Jaco Greeff <jacogr@gmail.com>
2020-11-26 11:06:46 +0100

    Bump deps

--- all-update.sh
+++ all-update.sh
@@ -40,45 +40,26 @@ echo "*** Updating inter-package-deps"
 ./dev/all-deps.js
 
 echo ""
-echo "*** Restoring updates to detached branches"
+echo "*** Installing updated packages"
 
 for REPO in "${DIRECTORIES[@]}"; do
   if [ "$REPO" != "" ] && [ -d "./$REPO/.git" ]; then
+    echo ""
     cd $REPO
 
     DETACHED=$(git status | grep "HEAD detached")
 
     if [ "$DETACHED" != "" ]; then
+      echo "*** Resetting $REPO"
+
       git reset --hard
+    else
+      echo "*** Installing $REPO"
+
+      yarn install | grep -v 'YN0013'
+      yarn dedupe
     fi
 
     cd ..
   fi
-done
-
-echo ""
-echo "*** Installing updated packages"
-
-for REPO in "${DIRECTORIES[@]}"; do
-  echo ""
-  echo "*** Installing in $REPO"
-
-  cd $REPO
-
-  CURRENT=$(git rev-parse --abbrev-ref HEAD)
-
-  # yarn config set registry "https://npm.pkg.github.com/"
-  if [ -f ".yarnrc.yml" ]; then
-    # yarn 2, assuming we only use those there
-    yarn install | grep -v 'YN0013'
-  else
-    yarn --ignore-engines
-  fi
-
-  if [ "$CURRENT" = "master" ]; then
-    # check if we have stuff updated, run tests & checks
-    echo "*** Performing master checks"
-  fi
-
-  cd ..
 done
