0b8cc955cb05e7097aed1a96704f105a78275892
Ewa Kowalska <30404388+ekowalsk@users.noreply.github.com>
2020-12-01 17:50:38 +0100

    Add fix flag to eslint script (#419)
    * Add fix flag to eslint script
    
    * Update packages/dev/scripts/polkadot-dev-run-lint.cjs
    
    Co-authored-by: Jaco Greeff <jacogr@gmail.com>

--- packages/dev/scripts/polkadot-dev-run-lint.cjs
+++ packages/dev/scripts/polkadot-dev-run-lint.cjs
@@ -11,6 +11,10 @@ const argv = require('yargs')
     'skip-tsc': {
       description: 'Skips running tsc',
       type: 'boolean'
+    },
+    'fix': {
+      description: 'Apply lint fixes',
+      type: 'boolean'
     }
   })
   .strict()
@@ -21,7 +25,7 @@ const execSync = require('./execSync.cjs');
 console.log('$ polkadot-dev-run-lint', process.argv.slice(2).join(' '));
 
 if (!argv['skip-eslint']) {
-  execSync(`yarn polkadot-exec-eslint --resolve-plugins-relative-to ${__dirname} --ext .js,.ts,.tsx ${process.cwd()}`);
+  execSync(`yarn polkadot-exec-eslint ${argv['fix'] ? '--fix ' : ''} --resolve-plugins-relative-to ${__dirname} --ext .js,.ts,.tsx ${process.cwd()}`);
 }
 
 if (!argv['skip-tsc']) {
