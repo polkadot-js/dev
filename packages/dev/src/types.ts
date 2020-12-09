b8ab57e82afdf2ff8f0f9ea3688fbbdfbafb010e
Jaco Greeff <jacogr@gmail.com>
2020-12-09 17:16:50 +0100

    Ensure .d.ts is copied

--- packages/dev/src/types.ts
+++ packages/dev/src/types.ts
@@ -1,6 +1,10 @@
 // Copyright 2017-2020 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { ApiPromise } from '@polkadot/api';
+
 export type EchoString = 'string';
 
 export type BlahType = number;
+
+export type WrappedApi = ApiPromise;
