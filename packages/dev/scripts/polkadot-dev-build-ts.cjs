fd64700b1505ee64fab57a0304b9c0cd71e563f8
Jaco Greeff <jacogr@gmail.com>
2020-12-09 14:15:39 +0100

    Build esm alongside cjs

--- packages/dev/scripts/polkadot-dev-build-ts.cjs
+++ packages/dev/scripts/polkadot-dev-build-ts.cjs
@@ -17,30 +17,63 @@ const CPX = ['css', 'gif', 'hbs', 'jpg', 'js', 'json', 'png', 'svg', 'd.ts']
 
 console.log('$ polkadot-dev-build-ts', process.argv.slice(2).join(' '));
 
+// webpack build
 function buildWebpack () {
   execSync('yarn polkadot-exec-webpack --config webpack.config.js --mode production');
 }
 
-async function buildBabel (dir) {
+// compile via babel, either via supplied config or default
+async function buildBabel (dir, type) {
+  const buildDir = `build${type === 'esm' ? '/esm' : ''}`;
   const configs = CONFIGS.map((c) => path.join(process.cwd(), `../../${c}`));
-  const babelConfig = configs.find((f) => fs.existsSync(f)) || configs[0];
+  const outDir = path.join(process.cwd(), buildDir);
 
   await babel({
     babelOptions: {
-      configFile: babelConfig
+      configFile: type === 'esm'
+        ? path.join(__dirname, '../config/babel-config-esm.cjs')
+        : configs.find((f) => fs.existsSync(f)) || path.join(__dirname, '../config/babel-config-cjs.cjs')
     },
     cliOptions: {
       extensions: ['.ts', '.tsx'],
       filenames: ['src'],
       ignore: '**/*.d.ts',
-      outDir: path.join(process.cwd(), 'build'),
+      outDir,
       outFileExtension: '.js'
     }
   });
 
   [...CPX]
     .concat(`../../build/${dir}/src/**/*.d.ts`, `../../build/packages/${dir}/src/**/*.d.ts`)
-    .forEach((src) => copySync(src, 'build'));
+    .forEach((src) => copySync(src, buildDir));
+
+  // rewrite a skeleton package.json with a type=module
+  if (type === 'esm') {
+    const pkgPath = path.join(outDir, 'package.json');
+    const pkg = require(pkgPath);
+    const base = {};
+
+    // overrides
+    if (pkg.browser) base.browser = pkg.browser;
+    if (pkg.main) base.main = pkg.main;
+    if (pkg['react-native']) base['react-native'] = pkg['react-native'];
+
+    fs.writeFileSync(pkgPath, JSON.stringify({
+      ...base,
+      author: pkg.author,
+      license: pkg.license,
+      name: pkg.name,
+      repository: pkg.repository,
+      sideEffects: pkg.sideEffects || false,
+      type: 'module',
+      version: pkg.version
+    }, null, 2));
+  }
+}
+
+// iterate through all the files that have been built, creating an exports map
+function buildExports (dir) {
+  // nothing atm
 }
 
 async function buildJs (dir) {
@@ -58,7 +91,10 @@ async function buildJs (dir) {
     if (fs.existsSync(path.join(process.cwd(), 'public'))) {
       buildWebpack(dir);
     } else {
-      await buildBabel(dir);
+      await buildBabel(dir, 'cjs');
+      await buildBabel(dir, 'esm');
+
+      buildExports();
     }
 
     console.log();
