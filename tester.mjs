101127bb76709e089076b24f8e6a73fe12350b6f
Jaco Greeff <jacogr@gmail.com>
2020-12-13 09:02:41 +0100

    Remove reference to old removed lerna script

--- tester.mjs
+++ tester.mjs
@@ -1,4 +1,4 @@
 // Copyright 2017-2020 @polkadot/util authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import './packages/dev/build/tester.js';
+import './packages/dev/build/tester.mjs';
