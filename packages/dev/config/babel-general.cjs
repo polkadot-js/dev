34c6e4e8c5ec61734c578446828992b1774f2e93
Jaco Greeff <jacogr@gmail.com>
2020-12-18 12:02:09 +0100

    Remove comment mangling, stick to defaults

--- packages/dev/config/babel-general.cjs
+++ packages/dev/config/babel-general.cjs
@@ -2,9 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = {
-  generatorOpts: {
-    shouldPrintComment: (value) =>
-      value.includes('@polkadot') ||
-      value.includes('SPDX-License-Identifier')
-  }
+  // This is not a great idea - it basically gets rid of the /** ... */
+  // comments that allows usage info, which well, is quite useful
+  // generatorOpts: {
+  //   shouldPrintComment: (value) =>
+  //     value.includes('@polkadot') ||
+  //     value.includes('SPDX-License-Identifier')
+  // }
 };
