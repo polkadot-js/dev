f1a977c5af753c22d105bfdd270dec6bdc654e9d
Jaco Greeff <jacogr@gmail.com>
2021-05-30 22:21:48 +0300

    lock.yml

--- all-bump-berry.sh
+++ all-bump-berry.sh
@@ -2,16 +2,16 @@
 # Copyright 2017-2021 @polkadot/dev authors & contributors
 # SPDX-License-Identifier: Apache-2.0
 
-DIRECTORIES=( "ts" "wasm" "common" "api" "docs" "ui" "phishing" "extension" "tools" "apps" ) # "client" )
+DIRECTORIES=( "ts" "wasm" "common" "api" "docs" "ui" "phishing" "extension" "tools" "apps" )
 
 for PKG in "${DIRECTORIES[@]}"; do
   echo "*** Updating yarn in $PKG"
-  rm -rf $PKG/.yarn/plugins $PKG/.yarn/releases
-  cp -R dev/.yarn/plugins $PKG/.yarn
-  cp -R dev/.yarn/releases $PKG/.yarn
-  cat dev/.yarnrc.yml > $PKG/.yarnrc.yml
-
   cd $PKG
+  git pull
+  rm -rf .yarn/plugins .yarn/releases
+  cp -R ../dev/.yarn/plugins .yarn
+  cp -R ../dev/.yarn/releases .yarn
+  cat ../dev/.yarnrc.yml > .yarnrc.yml
   yarn
   cd ..
 done
