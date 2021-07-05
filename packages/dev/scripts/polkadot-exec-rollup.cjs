32a489a34bce47584cfbe3540c12b7a32e12db21
Jaco Greeff <jacogr@gmail.com>
2021-07-05 11:25:58 +0300

    Add rollup execution

---
+++ packages/dev/scripts/polkadot-exec-rollup.cjs
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// Copyright 2017-2021 @polkadot/dev authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// const execSync = require('./execSync.cjs');
+
+// execSync(`yarn pnpify tsc ${process.argv.slice(2).join(' ')}`);
+
+console.log('$ rollup', process.argv.slice(2).join(' '));
+
+require('dist/bin/rollup');
