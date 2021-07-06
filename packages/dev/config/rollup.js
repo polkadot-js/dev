0a56b0c412becd3161e76a7811a7550d1258bf65
Jaco Greeff <jacogr@gmail.com>
2021-07-06 08:24:42 +0300

    rollup createBundle

--- packages/dev/config/rollup.js
+++ packages/dev/config/rollup.js
@@ -7,26 +7,33 @@ import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import fs from 'fs';
+import path from 'path';
+
+function sanitizePkg (pkg) {
+  return pkg.replace('@polkadot/', '');
+}
 
 function createName (input) {
-  return `polkadot-${input.replace('@polkadot/', '')}`
+  return `polkadot-${sanitizePkg(input)}`
     .toLowerCase()
     .replace(/[^a-zA-Z0-9]+(.)/g, (_, c) => c.toUpperCase());
 }
 
 export function createInput (pkg, index = 'index.js') {
-  return `packages/${pkg}/build/${index}`;
+  return `packages/${sanitizePkg(pkg)}/build/${index}`;
 }
 
-export function createOutput (pkg, external, globals = {}) {
-  const name = createName(pkg);
+export function createOutput (_pkg, external, globals = {}) {
+  const name = createName(_pkg);
+  const pkg = sanitizePkg(_pkg);
 
   return {
     file: `packages/${pkg}/build/bundle/${name}.js`,
     format: 'iife',
     globals: external.reduce((all, pkg) => ({
-      ...all,
-      [pkg]: createName(pkg)
+      [pkg]: createName(pkg),
+      ...all
     }), { ...globals }),
     name,
     preferConst: true
@@ -40,4 +47,25 @@ export function createPlugins (entries = []) {
     nodeResolve({ browser: true }),
     commonjs()
   ];
+}
+
+export function createBundle ({ entries = [], external, index = 'index.js', pkg }) {
+  return {
+    external,
+    input: createInput(pkg, index),
+    output: createOutput(pkg, external),
+    plugins: createPlugins(
+      external
+        .filter((p) =>
+          fs.existsSync(path.join(process.cwd(), 'packages', sanitizePkg(p)))
+        )
+        .reduce((all, p) => [
+          ...all,
+          {
+            find: `${p}/packageInfo`,
+            replacement: `../../${sanitizePkg(p)}/build/packageInfo.js`
+          }
+        ], [...entries])
+    )
+  };
 }
