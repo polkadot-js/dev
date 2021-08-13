a17787b7b632992aa95fdd29235e740306de9b2d
Jaco <jacogr@gmail.com>
2021-08-13 13:32:36 +0300

    Align loose mode on @babel/plugin-proposal-private-property-in-object (#476)

--- packages/dev/config/babel-plugins.cjs
+++ packages/dev/config/babel-plugins.cjs
@@ -18,6 +18,7 @@ module.exports = function (isEsm, withExt) {
     // ordering important, decorators before class properties
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-numeric-separator',
     '@babel/plugin-proposal-object-rest-spread',
