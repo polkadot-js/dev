f67280d1186e7e20cdb9f08b495da89ff29600a8
Jaco Greeff <jacogr@gmail.com>
2021-09-14 12:30:28 +0300

    Bump deps

--- packages/dev/config/jest.cjs
+++ packages/dev/config/jest.cjs
@@ -5,15 +5,26 @@ const fs = require('fs');
 const { defaults } = require('jest-config');
 
 module.exports = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  modulePathIgnorePatterns: fs
-    .readdirSync('packages')
-    .filter((p) => fs.statSync(`packages/${p}`).isDirectory())
-    .map((p) => `<rootDir>/packages/${p}/build`),
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: [
+    ...defaults.moduleFileExtensions,
+    'ts',
+    'tsx'
+  ],
+  modulePathIgnorePatterns: ['<rootDir>/build'].concat(
+    fs
+      .readdirSync('packages')
+      .filter((p) => fs.statSync(`packages/${p}`).isDirectory())
+      .map((p) => `<rootDir>/packages/${p}/build`)
+  ),
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': require.resolve('babel-jest')
   },
-  transformIgnorePatterns: ['/node_modules/(?!@polkadot|@babel/runtime/helpers/esm/)'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!@polkadot|@babel/runtime/helpers/esm/)'
+  ],
   verbose: true
 };
