5bbafd9e0e1d469e0ce418f4dc741bfdff0ce0c7
Jaco <jacogr@gmail.com>
2021-10-17 12:09:09 +0300

    Use requireBin (#491)
    * Use requireBin
    
    * named function
    
    * revert tsc
    
    * revert rollup
    
    * eslint without extension

--- packages/dev/scripts/polkadot-exec-prettier.cjs
+++ packages/dev/scripts/polkadot-exec-prettier.cjs
@@ -2,6 +2,4 @@
 // Copyright 2017-2021 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-console.log('$ prettier', process.argv.slice(2).join(' '));
-
-require('prettier/bin-prettier');
+require('./requireBin.cjs')('prettier', 'prettier/bin-prettier');
--- packages/dev/scripts/polkadot-exec-tsc.cjs
+++ packages/dev/scripts/polkadot-exec-tsc.cjs
@@ -2,10 +2,6 @@
 // Copyright 2017-2021 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-// const execSync = require('./execSync.cjs');
-
-// execSync(`yarn pnpify tsc ${process.argv.slice(2).join(' ')}`);
-
 console.log('$ tsc', process.argv.slice(2).join(' '));
 
 require('typescript/lib/tsc');
