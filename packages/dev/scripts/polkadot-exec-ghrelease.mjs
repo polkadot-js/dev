4c36bf01992dd9c844964bfc2f89001f87725771
Jaco <jacogr@gmail.com>
2021-11-04 14:48:20 +0100

    Only use import.cjs (#508)

--- packages/dev/scripts/polkadot-exec-ghrelease.mjs
+++ packages/dev/scripts/polkadot-exec-ghrelease.mjs
@@ -2,8 +2,6 @@
 // Copyright 2017-2021 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { importRelative } from './import.mjs';
+import { importRelative } from './import.cjs';
 
-importRelative('gh-release', 'gh-release/bin/cli.js').then(() =>
-  process.exit(0)
-);
+importRelative('gh-release', 'gh-release/bin/cli.js');
