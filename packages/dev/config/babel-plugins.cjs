3ce306200a5d033c1afa3deabb3c82ed38d34005
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2021-11-04 07:19:18 +0100

    Remove redundant babel plugins (#501)

--- packages/dev/config/babel-plugins.cjs
+++ packages/dev/config/babel-plugins.cjs
@@ -16,12 +16,8 @@ module.exports = function (isEsm, withExt) {
 
   return resolver([
     // ordering important, decorators before class properties
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-optional-chaining',
     ['@babel/plugin-transform-runtime', { useESModules: isEsm }],
     '@babel/plugin-syntax-bigint',
