1a43dde5fd83c017d0f52a8f58ce0263d4a979ef
Jaco <jacogr@gmail.com>
2021-11-05 07:06:07 +0200

    Exit gh-pages on all paths

--- packages/dev/scripts/polkadot-exec-ghpages.mjs
+++ packages/dev/scripts/polkadot-exec-ghpages.mjs
@@ -4,6 +4,9 @@
 
 import { importRelative } from './import.cjs';
 
-importRelative('gh-pages', 'gh-pages/bin/gh-pages.js')(process.argv).then(() =>
-  process.stdout.write('Published\n')
-);
+importRelative('gh-pages', 'gh-pages/bin/gh-pages.js')(process.argv)
+  .then(() => {
+    process.stdout.write('Published\n');
+    process.exit(0);
+  })
+  .catch(() => process.exit(1));
