867eed82f79846bd26b3b83255991f77910b2d7e
Jaco <jacogr@gmail.com>
2021-12-01 09:24:06 +0100

    Bump deps (#517)

--- packages/dev/scripts/polkadot-dev-circular.mjs
+++ packages/dev/scripts/polkadot-dev-circular.mjs
@@ -2,7 +2,6 @@
 // Copyright 2017-2021 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import chalk from 'chalk';
 import madge from 'madge';
 
 console.log('$ polkadot-dev-circular', process.argv.slice(2).join(' '));
@@ -12,20 +11,20 @@ madge('./', { fileExtensions: ['ts', 'tsx'] })
     const circular = res.circular();
 
     if (circular.length) {
-      process.stdout.write(chalk.red.bold(`Found ${circular.length} circular dependencies\n`));
+      process.stdout.write(`Found ${circular.length} circular dependencies\n`);
     } else {
-      process.stdout.write(chalk.bold('No circular dependency found!\n'));
+      process.stdout.write('No circular dependency found!\n');
     }
 
     circular.forEach((path, idx) => {
-      process.stdout.write(chalk.dim(`${(idx + 1).toString().padStart(4)}: `));
+      process.stdout.write(`${(idx + 1).toString().padStart(4)}: `);
 
       path.forEach((module, idx) => {
         if (idx) {
-          process.stdout.write(chalk.dim(' > '));
+          process.stdout.write(' > ');
         }
 
-        process.stdout.write(chalk.cyan.bold(module));
+        process.stdout.write(module);
       });
 
       process.stdout.write('\n');
