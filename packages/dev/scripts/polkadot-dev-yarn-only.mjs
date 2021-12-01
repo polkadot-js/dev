f7c917d33da4d7f5c7564276426b2a2ff242ec3d
Jaco <jacogr@gmail.com>
2021-12-01 10:16:29 +0200

    Bump deps

--- packages/dev/scripts/polkadot-dev-yarn-only.mjs
+++ packages/dev/scripts/polkadot-dev-yarn-only.mjs
@@ -2,8 +2,6 @@
 // Copyright 2017-2021 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import chalk from 'chalk';
-
 if (process.env.npm_execpath.includes('yarn')) {
   process.exit(0);
 }
@@ -11,12 +9,10 @@ if (process.env.npm_execpath.includes('yarn')) {
 const blank = ''.padStart(75);
 
 console.error(
-  chalk.white.bold.bgRed(
-    `${blank}\n   ${chalk.bold('FATAL: The use of yarn is required, install via npm is not supported.')}   \n${blank}`
-  )
+  `${blank}\n   FATAL: The use of yarn is required, install via npm is not supported.   \n${blank}`
 );
 console.error(`
-    Technical explanation: All the projects in the ${chalk.bold('@polkadot')} family use
+    Technical explanation: All the projects in the @polkadot' family use
     yarn workspaces, along with hoisting of dependencies. Currently only
     yarn supports package.json workspaces, hence the limitation.
 
