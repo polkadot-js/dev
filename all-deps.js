b369894edddd7f8ed9257dd740480511e9457e2d
Jaco <jacogr@gmail.com>
2022-01-01 17:28:39 +0100

    2022 (#553)

--- all-deps.js
+++ all-deps.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// Copyright 2017-2021 @polkadot/dev authors & contributors
+// Copyright 2017-2022 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 const fs = require('fs');
