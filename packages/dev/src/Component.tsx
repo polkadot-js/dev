602035b45add997be324aa1182484ef37b4c2e5b
Jaco <jacogr@gmail.com>
2022-06-12 13:25:09 +0200

    0.67.1 (#686)
    * 0.67.1
    
    * React imports

--- packages/dev/src/Component.tsx
+++ packages/dev/src/Component.tsx
@@ -1,7 +1,7 @@
 // Copyright 2017-2022 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
+import * as React from 'react';
 
 interface Props {
   children: React.ReactNode;
