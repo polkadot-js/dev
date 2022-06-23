42ba03344e5eea33384287d9aa8ff14ef39a8022
Jaco <jacogr@gmail.com>
2022-06-23 08:45:47 +0200

    Don't process.exit on execSync (#720)

--- packages/dev/scripts/execSync.mjs
+++ packages/dev/scripts/execSync.mjs
@@ -6,9 +6,5 @@ import cp from 'child_process';
 export default function execute (cmd, noLog) {
   !noLog && console.log(`$ ${cmd}`);
 
-  try {
-    cp.execSync(cmd, { stdio: 'inherit' });
-  } catch (error) {
-    process.exit(-1);
-  }
+  cp.execSync(cmd, { stdio: 'inherit' });
 }
