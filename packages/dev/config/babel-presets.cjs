36064ddda7323272d624eb53259a93ad78c643ff
Jaco <jacogr@gmail.com>
2022-07-01 15:05:19 +0300

    Swap Babel preset Node version to 14

--- packages/dev/config/babel-presets.cjs
+++ packages/dev/config/babel-presets.cjs
@@ -7,14 +7,14 @@ const CONFIG_CJS = {
   modules: 'commonjs',
   targets: {
     browsers: '>0.25% and last 2 versions and not ie 11 and not OperaMini all',
-    node: '12'
+    node: '14'
   }
 };
 
 const CONFIG_ESM = {
   modules: false,
   targets: {
-    node: '12'
+    node: '14'
   }
 };
 
