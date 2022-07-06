48531d80df9e202cef356bd4a1567f190737bbb6
Jaco <jacogr@gmail.com>
2022-07-06 12:23:50 +0300

    Allow for dynamic import code generation

--- packages/dev/config/babel-presets.cjs
+++ packages/dev/config/babel-presets.cjs
@@ -3,21 +3,6 @@
 
 const resolver = require('./babel-resolver.cjs');
 
-const CONFIG_CJS = {
-  modules: 'commonjs',
-  targets: {
-    browsers: '>0.25% and last 2 versions and not ie 11 and not OperaMini all',
-    node: '14'
-  }
-};
-
-const CONFIG_ESM = {
-  modules: false,
-  targets: {
-    node: '14'
-  }
-};
-
 module.exports = function (isEsm) {
   return resolver([
     '@babel/preset-typescript',
@@ -25,6 +10,24 @@ module.exports = function (isEsm) {
       development: false,
       runtime: 'automatic'
     }],
-    ['@babel/preset-env', isEsm ? CONFIG_ESM : CONFIG_CJS]
+    ['@babel/preset-env', {
+      exclude: [
+        // we don't want 2n ** 128n to Math.pow(2n, 128n)
+        '@babel/plugin-transform-exponentiation-operator',
+        // we don't want await import(...) to Promise.resolve(require(...))
+        'proposal-dynamic-import'
+      ],
+      modules: isEsm
+        ? false
+        : 'commonjs',
+      targets: isEsm
+        ? {
+          node: '14'
+        }
+        : {
+          browsers: '>0.25% and last 2 versions and not ie 11 and not OperaMini all',
+          node: '14'
+        }
+    }]
   ]);
 };
