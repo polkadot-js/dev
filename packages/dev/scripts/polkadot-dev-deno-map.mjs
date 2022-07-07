ff6f367fd450dcbef28b9c191fe06bb7916249ce
Jaco <jacogr@gmail.com>
2022-07-07 17:27:20 +0200

    Auto-create deno directory (#743)
    * Auto-create deno directory
    
    * Remove deno folder

--- packages/dev/scripts/polkadot-dev-deno-map.mjs
+++ packages/dev/scripts/polkadot-dev-deno-map.mjs
@@ -3,16 +3,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import fs from 'fs';
+import mkdirp from 'mkdirp';
 
 const [exports, imports] = fs
   .readdirSync('packages')
   .filter((p) => fs.existsSync(`packages/${p}/src/mod.ts`))
   .reduce(([exports, imports], p) => {
-    exports.push(`export * as ${p.replace('-', '_')} from 'https://deno.land/x/polkadot/${p}/mod.ts';`);
+    exports.push(`export * as ${p.replace(/-/g, '_')} from 'https://deno.land/x/polkadot/${p}/mod.ts';`);
     imports[`https://deno.land/x/polkadot/${p}/`] = `../packages/${p}/build-deno/`;
 
     return [exports, imports];
   }, [[], {}]);
+
+mkdirp.sync('deno');
 
 fs.writeFileSync('deno/mod.ts', `// Copyright 2017-${new Date().getFullYear()} @polkadot/dev authors & contributors\n// SPDX-License-Identifier: Apache-2.0\n\n// auto-generated via polkadot-dev-deno-map, do not edit\n\n${exports.sort().join('\n')}\n`);
 
