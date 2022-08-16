1c04862766cbd6ed000e74a1574ab645ce0cb038
Jaco <jacogr@gmail.com>
2022-08-16 11:53:34 +0300

    Test for tsx imports

---
+++ packages/dev/src/Root.tsx
@@ -0,0 +1,25 @@
+// Copyright 2017-2022 @polkadot/dev authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import * as React from 'react';
+
+import Component from './Component.js';
+
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+  label?: string;
+}
+
+function Root ({ children, className, label }: Props): React.ReactElement<Props> {
+  return (
+    <Component
+      className={className}
+      label={label}
+    >
+      {children}
+    </Component>
+  );
+}
+
+export default React.memo(Root);
