06093378a03af63c9d41694d5a6c22578e8a6798
Jaco <jacogr@gmail.com>
2022-08-16 13:05:48 +0300

    Adjust with credits

--- packages/dev/config/jest-resolver.cjs
+++ packages/dev/config/jest-resolver.cjs
@@ -1,22 +1,27 @@
 // Copyright 2017-2022 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-const resolutions = [
+// Adapted from https://github.com/VitorLuizC/ts-jest-resolver/blob/e35fcd388f6503b5bcb701a216a4beb2df1e1f7e/src/index.ts#L1
+
+// MIT License
+// Copyright (c) 2021 Vitor Luiz Cavalcanti
+
+const tests = [
   {
-    extensions: ['.tsx', '.ts', '.js'],
-    matcher: /\.jsx?$/i
+    exts: ['.tsx', '.ts', '.js'],
+    re: /\.jsx?$/i
   }
 ];
 
 module.exports = function (path, options) {
-  const resolution = resolutions.find(({ matcher }) => matcher.test(path));
+  const { exts, re } = tests.find(({ re }) => re.test(path)) || { exts: [] };
 
-  if (resolution) {
-    for (const extension of resolution.extensions) {
+  if (re && exts.length) {
+    for (let i = 0; i < exts.length; i++) {
       try {
-        return options.defaultResolver(path.replace(resolution.matcher, extension), options);
+        return options.defaultResolver(path.replace(re, exts[i]), options);
       } catch {
-        continue;
+        // ignore
       }
     }
   }
