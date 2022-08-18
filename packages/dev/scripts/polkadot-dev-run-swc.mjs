b34ddf93ea8743ef657a61936f098bc7a212593f
Jaco <jacogr@gmail.com>
2022-08-18 13:58:06 +0300

    swc exec helpers

---
+++ packages/dev/scripts/polkadot-dev-run-swc.mjs
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+// Copyright 2017-2022 @polkadot/dev authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import path from 'path';
+import yargs from 'yargs';
+
+import { __dirname } from './dirname.mjs';
+import { execSync } from './execute.mjs';
+
+console.log('$ polkadot-dev-run-swc', process.argv.slice(2).join(' '));
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    type: {
+      description: 'The type of compilation, either cjs or esm',
+      type: 'string'
+    }
+  })
+  .strict()
+  .argv;
+
+const type = argv.type === 'cjs'
+  ? 'cjs'
+  : 'esm';
+
+execSync(`yarn polkadot-exec-swc --delete-dir-on-start --copy-files --config-file ${path.join(__dirname, `../config/swcrc-${type}`)} --out-dir build-swc-${type} src`);
