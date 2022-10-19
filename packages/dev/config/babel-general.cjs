a0961c152b8eda98137f0ddaeefcc8a9074a9038
Jaco <jacogr@gmail.com>
2022-10-19 07:40:19 +0300

    Bump deps

--- packages/dev/config/babel-general.cjs
+++ packages/dev/config/babel-general.cjs
@@ -3,7 +3,11 @@
 
 module.exports = {
   assumptions: {
+    // ensure that ?. & ?? uses !=/== null checks (not also undefined)
+    noDocumentAll: true,
+    // no extra defineProperty for private class fields
     privateFieldsAsProperties: true,
+    // no extra defineProperty for public class fields
     setPublicClassFields: true
   }
   // Really want to switch this on, but not as of yet...
