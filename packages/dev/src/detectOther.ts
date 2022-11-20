63f5e1443dd4471e1b60b5da5655851867fd223c
Jaco <jacogr@gmail.com>
2022-11-20 13:20:13 +0200

    Remove cross dep

--- packages/dev/src/detectOther.ts
+++ packages/dev/src/detectOther.ts
@@ -1,9 +1,4 @@
 // Copyright 2017-2022 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-// samples...
-// var _packageInfo = require("@polkadot/networks/packageInfo");
-// import { packageInfo as netInfo } from '@polkadot/networks/packageInfo';
-// import { packageInfo as utilInfo } from '@polkadot/util';
-
 export default [];
