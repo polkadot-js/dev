7ca92beaa2bd9df660daba6bdcc00351efb0c12d
Jaco <jacogr@gmail.com>
2023-02-10 14:08:45 +0200

    Adjust

--- packages/dev/src/Hidden.spec.tsx
+++ packages/dev/src/Hidden.spec.tsx
@@ -6,21 +6,21 @@
 
 import { fireEvent, render, screen } from '@testing-library/react';
 import { strict as assert } from 'node:assert';
-// import { test } from 'node:test';
+// import { describe, it } from 'node:test';
 import React from 'react';
 
 import Hidden from './Hidden';
 
-/* eslint-disable @typescript-eslint/no-floating-promises,@typescript-eslint/require-await */
+describe('react testing', () => {
+  it('shows the children when the checkbox is checked', () => {
+    const testMessage = 'Test Message';
 
-test('shows the children when the checkbox is checked', async () => {
-  const testMessage = 'Test Message';
+    render(<Hidden>{testMessage}</Hidden>);
 
-  render(<Hidden>{testMessage}</Hidden>);
+    assert.equal(screen.queryByText(testMessage), null);
 
-  assert.equal(screen.queryByText(testMessage), null);
+    fireEvent.click(screen.getByLabelText(/show/i));
 
-  fireEvent.click(screen.getByLabelText(/show/i));
-
-  assert.notEqual(screen.getByText(testMessage), null);
+    assert.notEqual(screen.getByText(testMessage), null);
+  });
 });
