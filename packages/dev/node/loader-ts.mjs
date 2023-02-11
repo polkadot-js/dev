b5260a799a1c14875c1e6cfa4ff2b83e2e6ab212
Jaco <jacogr@gmail.com>
2023-02-11 11:41:20 +0200

    Read tsconfig for aliases

--- packages/dev/node/loader-ts.mjs
+++ packages/dev/node/loader-ts.mjs
@@ -4,6 +4,7 @@
 // Adapted from: https://nodejs.org/api/esm.html#esm_transpiler_loader
 
 import { transform } from '@swc/core';
+import JSON5 from 'json5';
 import fs from 'node:fs';
 import path from 'node:path';
 import process from 'node:process';
@@ -13,34 +14,7 @@ const cwdPath = process.cwd();
 const cwdURL = pathToFileURL(cwdPath).href;
 const extensionsRegex = /\.ts$|\.tsx$/;
 const extensions = ['.ts', '.tsx'];
-const tsConfig = getTsConfig();
-
-// TODO We need to read, extend tsconfig.json and fill this in
-function getTsConfig () {
-  const config = {
-    basePath: './packages',
-    entries: [],
-    paths: {}
-  };
-
-  config.entries = Object
-    .entries(config.paths)
-    .map(([key, value]) => {
-      const filter = key.split(/[\\/]/);
-      const isStar = filter[filter.length - 1] === '*';
-
-      return {
-        filter,
-        isStar,
-        key,
-        path: isStar
-          ? path.join(config.basePath, ...filter.slice(0, -1))
-          : path.join(config.basePath, value)
-      };
-    });
-
-  return config;
-}
+const tsAlias = getTsAliases();
 
 function resolveRegex (specifier, parentURL) {
   // handle .ts extensions directly
@@ -91,7 +65,7 @@ function resolveRelative (specifier, parentURL) {
 
 function resolvePackages (specifier) {
   const parts = specifier.split(/[\\/]/);
-  const direct = tsConfig.entries
+  const direct = tsAlias
     .filter(({ isStar }) => !isStar)
     .find(({ filter }) =>
       parts.length === filter.length &&
@@ -143,4 +117,54 @@ async function compile (url, source) {
   });
 
   return result.code;
+}
+
+// fills in a tsconfig, taking the extends into account
+function readTsConfig (currentPath = cwdPath, tsconfig = 'tsconfig.json') {
+  const configPath = path.join(currentPath, tsconfig);
+
+  try {
+    const config = JSON5.parse(fs.readFileSync(configPath, 'utf8'));
+    const baseUrl = config.compilerOptions?.baseUrl || '.';
+    let paths = config.compilerOptions?.paths || {};
+
+    if (config.extends) {
+      const extRoot = config.extends.startsWith('.')
+        ? currentPath
+        : path.join(cwdPath, 'node_modules');
+      const extSubs = config.extends.split(/[\\/]/);
+      const extPath = path.join(extRoot, ...extSubs.slice(0, -1));
+      const extConfig = readTsConfig(extPath, extSubs[extSubs.length - 1]);
+
+      paths = { ...extConfig.paths, ...paths };
+    }
+
+    return { baseUrl, paths };
+  } catch (error) {
+    console.error(`FATAL: Error parsing ${configPath}:: ${error.message}`);
+
+    throw error;
+  }
+}
+
+// retrieves all ts aliases
+function getTsAliases () {
+  const { baseUrl, paths } = readTsConfig();
+
+  return Object
+    .entries(paths)
+    .map(([key, [value]]) => {
+      const filter = key.split(/[\\/]/);
+      const isStar = filter[filter.length - 1] === '*';
+
+      return {
+        filter: isStar
+          ? filter.slice(0, -1)
+          : filter,
+        isStar,
+        path: isStar
+          ? path.join(cwdPath, baseUrl, ...value.split(/[\\/]/).slice(0, -1))
+          : path.join(cwdPath, baseUrl, value)
+      };
+    });
 }
