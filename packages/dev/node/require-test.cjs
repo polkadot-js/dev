44f92173b4d3a9a8f33dc91e3f3c99377c524a10
Jaco <jacogr@gmail.com>
2023-02-11 16:46:02 +0200

    Adjust globals

--- packages/dev/node/require-test.cjs
+++ packages/dev/node/require-test.cjs
@@ -29,20 +29,9 @@ function emptyExpect (pre) {
 Object
   .entries(({
     // browser environment via JSDOM
-    ...{
-      ...['crypto', 'document', 'navigator'].reduce((env, k) => ({
-        ...env,
-        [k]: dom.window[k]
-      }), {}),
-      window: dom.window
-    },
+    ...['crypto', 'document', 'navigator'].reduce((env, k) => ({ ...env, [k]: dom.window[k] }), { window: dom.window }),
     // testing environment via node:test
-    ...{
-      afterAll: after,
-      afterEach,
-      beforeAll: before,
-      beforeEach
-    }
+    ...{ afterAll: after, afterEach, beforeAll: before, beforeEach }
   }))
   .forEach(([globalName, fn]) => {
     globalThis[globalName] = fn;
