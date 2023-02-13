b45882534f25ec9079add11e11379ff733618275
Jaco <jacogr@gmail.com>
2023-02-13 12:27:49 +0200

    TS types via JSDoc

--- packages/dev/node/require-test/util.cjs
+++ packages/dev/node/require-test/util.cjs
@@ -3,22 +3,31 @@
 
 /**
  * A simple helper to indicate unimplemented functionality
+ *
+ * @param {string} objName - The name of the top-level object
+ * @param {string} key - The key that we are extending
+ * @returns {(...args: unknown[]) => never}
  */
-function unimplemented (name, key) {
+function unimplemented (objName, key) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return (..._) => {
-    throw new Error(`${name}.${key}(...) has not been implemented`);
+    throw new Error(`${objName}.${key}(...) has not been implemented`);
   };
 }
 
 /**
  * Extends a given object with the named functions if they do not
  * already exist on the object.
+ *
+ * @param {string} objName - The name of the top-level object
+ * @param {string[]} keys - The keys that we are adding
+ * @param {Record<string, (...args: unknown[]) => unknown>} obj
+ * @returns {Record<string, (...args: unknown[]) => unknown>}
  */
-function unimplementedObj (name, keys, obj) {
+function unimplementedObj (objName, keys, obj) {
   keys.forEach((key) => {
     if (!obj[key]) {
-      obj[key] = unimplemented(name, key);
+      obj[key] = unimplemented(objName, key);
     }
   });
 
--- packages/dev/node/require-test/util.test.ts
+++ packages/dev/node/require-test/util.test.ts
@@ -15,26 +15,22 @@ describe('unimplemented', (): void => {
 
 describe('unimplementedObj', (): void => {
   it('throws on unimplemented values', (): void => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const test = unimplementedObj('obj', ['a', 'b'], {
       a: () => 'test'
     });
 
     expect(
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
       () => test.b()
     ).toThrow('obj.b(...) has not been implemented');
   });
 
   it('calls into implemented values', (): void => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const test = unimplementedObj('obj', ['a', 'b'], {
       b: () => 'test'
     });
 
     const spy = jest.spyOn(test, 'b');
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     test.b(1, 2, 3);
 
     expect(spy).toHaveBeenLastCalledWith(1, 2, 3);
