ce281a5168141af190c3eaccebe18c595dc96c9c
Jaco <jacogr@gmail.com>
2023-02-20 18:39:15 +0200

    Remove jest deps (#928)
    * Remove jest deps
    
    * Bump minor
    
    * Bumps
    
    * Adjust test command

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2023 @polkadot/util authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-const config = require('@polkadot/dev/config/jest.cjs');
-
-module.exports = {
-  ...config,
-  testEnvironment: 'jsdom'
-};
--- packages/dev/scripts/polkadot-exec-node-test.mjs
+++ packages/dev/scripts/polkadot-exec-node-test.mjs
@@ -6,8 +6,11 @@ import TapParser from 'tap-parser';
 
 console.time('\t elapsed :');
 
+const WITH_DEBUG = false;
+
 const files = process.argv.slice(2);
 const stats = {
+  diag: [],
   fail: [],
   pass: [],
   skip: [],
@@ -29,7 +32,7 @@ function output (ch) {
   process.stdout.write(ch);
 }
 
-function indent (str) {
+function indent (str = '') {
   return `\t\t\t ${
     str
       .replaceAll('\n', '[&&*&&]')
@@ -37,51 +40,67 @@ function indent (str) {
   }`;
 }
 
-run({
-  files,
-  timeout: 60_000
-}).pipe(
-  new TapParser(() => {
-    process.stdout.write('\n');
+const parser = new TapParser(() => {
+  process.stdout.write('\n');
 
-    stats.fail.forEach((r) => {
-      console.log();
-      console.log(/* '\t✕' */ '\tx', r.fullname);
-      console.log();
-      console.log('\t\t', r.name);
-      console.log();
-      console.log(indent(`${r.diag.failureType} / ${r.diag.code}`));
-      console.log();
-      console.log(indent(r.diag.error));
-      console.log();
-      console.log(indent(r.diag.stack));
-    });
+  stats.fail.forEach((r) => {
+    WITH_DEBUG && console.error(r);
 
     console.log();
-    console.log('\t  passed ::', stats.pass.length);
-    console.log('\t  failed ::', stats.fail.length);
-    console.log('\t skipped ::', stats.skip.length);
-    console.log('\t    todo ::', stats.todo.length);
-    console.log('\t   total ::', stats.total);
-    console.timeEnd('\t elapsed :');
+    console.log('\tx', r.fullname.replaceAll('\n', ' '));
     console.log();
+    console.log('\t\t', r.name);
+    console.log();
+    console.log(indent(`${r.diag.failureType} / ${r.diag.code}`));
+    console.log();
+    console.log(indent(r.diag.error));
+    console.log();
+    console.log(indent(r.diag.stack));
+  });
 
-    process.exit(stats.fail.length);
+  console.log();
+  console.log('\t  passed ::', stats.pass.length);
+  console.log('\t  failed ::', stats.fail.length);
+  console.log('\t skipped ::', stats.skip.length);
+  console.log('\t    todo ::', stats.todo.length);
+  console.log('\t   total ::', stats.total);
+  console.timeEnd('\t elapsed :');
+  console.log();
+
+  if (stats.fail.length) {
+    stats.diag.forEach((e) => console.error(e));
+    console.error();
+  }
+
+  process.exit(stats.fail.length);
+});
+
+// just in-case, we want these logged
+// .on('bailout', (r) => console.error('bailout', r))
+// .on('error', (r) => console.error('error', r))
+// .on('extra', (r) => console.error('extra', r))
+// .on('plan', (r) => console.error('plan', r));
+
+parser
+  .on('fail', (r) => {
+    stats.fail.push(r);
+    output('x'); // '✕');
   })
-    .on('fail', (r) => {
-      stats.fail.push(r);
-      output('x'); // '✕');
-    })
-    .on('pass', (r) => {
-      stats.pass.push(r);
-      output('.'); // '·'); // '✔');
-    })
-    .on('skip', (r) => {
-      stats.skip.push(r);
-      output('='); // '⁃');
-    })
-    .on('todo', (r) => {
-      stats.todo.push(r);
-      output('*'); // '⁃');
-    })
-);
+  .on('pass', (r) => {
+    stats.pass.push(r);
+    output('.'); // '·'); // '✔');
+  })
+  .on('skip', (r) => {
+    stats.skip.push(r);
+    output('='); // '⁃');
+  })
+  .on('todo', (r) => {
+    stats.todo.push(r);
+    output('*'); // '⁃');
+  });
+
+run({ files, timeout: 60_000 })
+  .on('test:diagnostic', (r) => {
+    stats.diag.push(r);
+  })
+  .pipe(parser);
