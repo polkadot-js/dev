5e0dc6863f2610af5511c8673e96b1bfb77adfae
Jaco <jacogr@gmail.com>
2023-02-27 10:45:14 +0200

    Adjust for usage of new eslint ruleset

--- packages/dev/src/index.ts
+++ packages/dev/src/index.ts
@@ -1,7 +1,6 @@
 // Copyright 2017-2023 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-throw new Error('@polkadot/dev is not meant to be imported via root. Rather if provides a set of shared dependencies, a collection of scripts, base configs and some loaders accessed via the scripts. It is only meant to be used as a shared resource by all @polkadot/* projects');
-
-// intentionally left empty
 export {};
+
+throw new Error('@polkadot/dev is not meant to be imported via root. Rather if provides a set of shared dependencies, a collection of scripts, base configs and some loaders accessed via the scripts. It is only meant to be used as a shared resource by all @polkadot/* projects');
