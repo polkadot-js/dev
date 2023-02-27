2e5b9e5bc1cec2a994295b0e86b37b756d95561d
Jaco <jacogr@gmail.com>
2023-02-27 11:25:43 +0200

    Adjust eslint recommended rule imports

---
+++ packages/dev/config/eslint-recommended.cjs
@@ -0,0 +1,6 @@
+// Copyright 2017-2023 @polkadot/dev authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+const configJs = require('@eslint/js');
+
+module.exports = configJs.configs.recommended;
