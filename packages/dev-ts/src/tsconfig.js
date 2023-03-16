eb90eabfe44a4bd56a42df6cdebc3736bc7d0f2a
Jaco <jacogr@gmail.com>
2023-03-16 08:23:59 +0200

    Cleanup tsconfig file parsing (#979)

--- packages/dev-ts/src/tsconfig.js
+++ packages/dev-ts/src/tsconfig.js
@@ -8,12 +8,11 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { pathToFileURL } from 'node:url';
 
-import { CWD_PATH, MOD_PATH } from './common.js';
+import { CWD_PATH, CWD_URL, MOD_PATH } from './common.js';
 
-/** @typedef {{ baseParentUrl: string; filter: string[]; isWildcard?: boolean; path: string }} Alias */
-/** @typedef {{ baseUrl?: string; paths?: Record<string, string[]> }} CompilerOptions */
-/** @typedef {{ compilerOptions?: CompilerOptions; extends?: string }} JsonConfig */
-/** @typedef {{ baseParentUrl: string; basePath: string; paths: Record<string, string[]> }} PartialConfig */
+/** @typedef {{ filter: string[]; isWildcard?: boolean; path: string, url: URL }} Alias */
+/** @typedef {{ compilerOptions?: { baseUrl?: string; paths?: Record<string, string[]> }; extends?: string }} JsonConfig */
+/** @typedef {{ paths: Record<string, string[]>; url?: URL }} PartialConfig */
 
 /**
  * @internal
@@ -22,34 +21,55 @@ import { CWD_PATH, MOD_PATH } from './common.js';
  *
  * @param {string} [currentPath]
  * @param {string} [tsconfig]
+ * @param {string} [fromFile]
  * @returns {PartialConfig}
  **/
-function readConfigFile (currentPath = CWD_PATH, tsconfig = 'tsconfig.json') {
-  const configPath = path.join(currentPath, tsconfig);
+function readConfigFile (currentPath = CWD_PATH, tsconfig = 'tsconfig.json', fromFile) {
+  const configFile = path.join(currentPath, tsconfig);
+
+  if (!fs.existsSync(configFile)) {
+    console.warn(`No ${configFile}${fromFile ? ` (extended from ${fromFile})` : ''} found, assuming defaults`);
+
+    return { paths: {} };
+  }
 
   try {
     /** @type {JsonConfig} */
-    const config = JSON5.parse(fs.readFileSync(configPath, 'utf8'));
+    const { compilerOptions, extends: parentConfig } = JSON5.parse(fs.readFileSync(configFile, 'utf8'));
 
     // ensure that the basePath does point to an actual directory
-    const basePath = config.compilerOptions?.baseUrl || '.';
-    let paths = config.compilerOptions?.paths || {};
+    /** @type {URL | undefined} */
+    let url;
 
-    if (config.extends) {
-      const extRoot = config.extends.startsWith('.')
-        ? currentPath
-        : MOD_PATH;
-      const extSubs = config.extends.split(/[\\/]/);
-      const extPath = path.join(extRoot, ...extSubs.slice(0, -1));
-      const extConfig = readConfigFile(extPath, extSubs.at(-1));
+    if (compilerOptions?.baseUrl) {
+      const configDir = path.dirname(configFile);
 
-      // base configs are overridden by later configs, order here matters
-      paths = { ...extConfig.paths, ...paths };
+      // the baseParentUrl is relative to the actual config file
+      url = pathToFileURL(path.join(configDir, `${compilerOptions.baseUrl}/`));
     }
 
-    return { baseParentUrl: pathToFileURL(path.join(CWD_PATH, basePath, '/')).href, basePath, paths };
+    // empty paths if none are found
+    let paths = compilerOptions?.paths || {};
+
+    if (parentConfig) {
+      const extRoot = parentConfig.startsWith('.')
+        ? currentPath
+        : MOD_PATH;
+      const extSubs = parentConfig.split(/[\\/]/);
+      const extPath = path.join(extRoot, ...extSubs.slice(0, -1));
+      const extConfig = readConfigFile(extPath, extSubs.at(-1), configFile);
+
+      // base configs are overridden by later configs, order here matters
+      // FIXME The paths would be relative to the baseUrl at that point... for
+      // now we don't care much since we define these 2 together in all @polkadot
+      // configs, but it certainly _may_ create and issue at some point (for others)
+      paths = { ...extConfig.paths, ...paths };
+      url = url || extConfig.url;
+    }
+
+    return { paths, url };
   } catch (error) {
-    console.error(`FATAL: Error parsing ${configPath}:: ${error.message}`);
+    console.error(`FATAL: Error parsing ${configFile}:: ${error.message}`);
 
     throw error;
   }
@@ -63,12 +83,13 @@ function readConfigFile (currentPath = CWD_PATH, tsconfig = 'tsconfig.json') {
  * @returns {Alias[]}
  **/
 function extractAliases () {
-  const { baseParentUrl, paths } = readConfigFile();
+  const { paths, url = CWD_URL } = readConfigFile();
 
   return Object
     .entries(paths)
-    // The path value is an array - we only handle the first entry in there,
-    // this is a possible fix into the future if it is ever an issue...
+    // TODO The path value is an array - we only handle the first entry in there,
+    // this is a possible fix into the future if it is ever an issue... (may have
+    // some impacts on the actual loader where only 1 alias is retrieved)
     .map(([key, [path]]) => {
       const filter = key.split(/[\\/]/);
       const isWildcard = filter.at(-1) === '*';
@@ -79,12 +100,12 @@ function extractAliases () {
       }
 
       return {
-        baseParentUrl,
         filter: isWildcard
           ? filter.slice(0, -1)
           : filter,
         isWildcard,
-        path
+        path,
+        url
       };
     });
 }
