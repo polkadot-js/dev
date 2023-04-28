97d90b30e8c40a54679221f706bfc398b2ce7579
Jaco <jacogr@gmail.com>
2023-04-28 08:23:14 +0300

    Adjust output for private fields (#1029)

--- packages/dev/scripts/polkadot-dev-build-ts.mjs
+++ packages/dev/scripts/polkadot-dev-build-ts.mjs
@@ -50,7 +50,21 @@ async function compileJs (compileType, type) {
       files.forEach((filename) => {
         // split src prefix, replace .ts extension with .js
         const outFile = path.join(buildDir, filename.split(/[\\/]/).slice(1).join('/').replace(/\.tsx?$/, '.js'));
-        const { outputText } = ts.transpileModule(fs.readFileSync(filename, 'utf-8'), {
+
+        // Until we hit the es2022 target, all private fields are compiled to using
+        // WeakMap with less than stellar performannce of get/set on the polyfill. We
+        // replace usages of these with TS-only private fields.
+        //
+        // As used these are internal-only, completely hidden fields should be done via
+        // closures, see e.g. the common keypairs where this is done
+        const source = fs
+          .readFileSync(filename, 'utf-8')
+          .replace(/(this|other|source)\.#/g, '$1.__internal__')
+          .replace(/ {2}(async|readonly) #/g, '  private $1 __internal__')
+          .replace(/ {2}#/g, '  private __internal__');
+
+        // compile with the options aligning with our tsconfig
+        const { outputText } = ts.transpileModule(source, {
           compilerOptions: {
             esModuleInterop: true,
             importHelpers: true,
