e6c6d63131f8188b33b5ccd57f52aa167f6d4b87
Jaco <jacogr@gmail.com>
2023-07-05 20:46:06 +0300

    Styled bump

--- packages/dev/src/rootJs/Jsx.tsx
+++ packages/dev/src/rootJs/Jsx.tsx
@@ -3,21 +3,12 @@
 
 // Adapted from https://github.com/testing-library/react-testing-library#basic-example
 
-import type { StyledInterface } from 'styled-components';
 import type { Props } from './JsxChild.js';
 
 import React, { useCallback, useState } from 'react';
-import styledComponents from 'styled-components';
+import { styled } from 'styled-components';
 
 import Child from './JsxChild.js';
-
-// The styled-components typings are problematic, especially in nodenext - here
-// we actually cater for both styled-componentsn 5.0 and 6.0 (where a named export exists)
-export const styled = (
-  (styledComponents as unknown as { styled: StyledInterface }).styled ||
-  (styledComponents as unknown as { default: StyledInterface }).default ||
-  styledComponents as unknown as StyledInterface
-);
 
 function Hidden ({ children, className }: Props): React.ReactElement<Props> {
   const [isMessageVisible, setMessageVisibility] = useState(false);
