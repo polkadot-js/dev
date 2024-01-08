4d37f62e2d919950b8dfe536331e429b14f7ffc2
Jaco <jacogr@gmail.com>
2024-01-08 15:00:29 +0200

    scripts & others

--- scripts/all-update.sh
+++ scripts/all-update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright 2017-2023 @polkadot/dev authors & contributors
+# Copyright 2017-2024 @polkadot/dev authors & contributors
 # SPDX-License-Identifier: Apache-2.0
 
 # This scripts updates all the inter polkadot-js dependencies. To do so it
