fe449672b572def73ddb8ff1f29c47f02d985555
tarikgul <tariksnow37@gmail.com>
2024-11-09 10:23:19 -0500

    Add test loader

---
+++ packages/dev-ts/src/testCached.ts
@@ -0,0 +1,13 @@
+// Copyright 2017-2024 @polkadot/dev-ts authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// Adapted from: https://nodejs.org/api/esm.html#esm_transpiler_loader
+//
+// NOTE: This assumes the loader implementation for Node.js >= 18
+
+import { loaderOptions } from './common.js';
+
+loaderOptions.isCached = true;
+
+export { load } from './testLoader.js';
+export { resolve } from './resolver.js';
