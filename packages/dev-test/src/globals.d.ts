46764eb786db5743f9a945b914afe1aa3d3abad4
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2025-01-02 17:06:17 -0800

    2025 (#1152)

--- packages/dev-test/src/globals.d.ts
+++ packages/dev-test/src/globals.d.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2024 @polkadot/dev-test authors & contributors
+// Copyright 2017-2025 @polkadot/dev-test authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 /* eslint-disable no-var */
--- packages/dev-test/src/index.ts
+++ packages/dev-test/src/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2024 @polkadot/dev-test authors & contributors
+// Copyright 2017-2025 @polkadot/dev-test authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 throw new Error('Use node --require @polkadot/dev-test/{node, browser} depending on the required environment');
