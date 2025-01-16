82bab52593e2e7885defe524068b0b095c770ab2
tarikgul <tariksnow37@gmail.com>
2025-01-16 13:11:02 -0500

    Add diagnostic parentPort back

--- packages/dev/scripts/polkadot-exec-node-test.mjs
+++ packages/dev/scripts/polkadot-exec-node-test.mjs
@@ -330,6 +330,7 @@ if (isMainThread) {
     .on('test:coverage', () => undefined)
     .on('test:diagnostic', (data) => {
       stats.diag.push(data);
+      parentPort && parentPort.postMessage({ data: stats, type: 'result' });
     })
     .on('test:fail', (/** @type {any} */ data) => {
       stats.fail.push(data);
